253b93325617e9663b43257d10d9657b3de3c84a
bichar4 <bichar4@gmail.com>
2021-09-06 11:11:48 -0500

    testing success  of sending json response

--- api/controllers/search-material.js
+++ api/controllers/search-material.js
@@ -1,4 +1,5 @@
-const axios = require("axios")
+const axios = require("axios");
+const { exists } = require("grunt");
 module.exports = {
 
 
@@ -23,21 +24,30 @@ module.exports = {
       description: 'Requesting user is a guest, so show the public landing page.',
       viewTemplatePath: 'pages/homepage.ejs'
     },
+    ok: {
+      responseType: 'ok'
+    },
 
   },
 
 
-  fn: async function (inputs) {
+  fn: async function (inputs, exits) {
     console.log("I am inside search material")
     console.log("I just for the value", inputs.queryString)
-
-    axios.get("https://www.materialsproject.org/rest/v2/materials/CaSi2/vasp/", { headers: { "x-api-key": "atfcnouGu4jdflvjap" } }).then((res) => {
-      console.log("My response", res.data)
-
-    }).catch((err) => {
-      console.log("This is an error", err)
-    })
-    // All done.
+    axios.get("https://www.materialsproject.org/rest/v2/materials/" + inputs.queryString + "/vasp/",
+      {
+        headers: {
+          "x-api-key": "atfcnouGu4jdflvjap"
+        }
+      })
+      .then((res) => {
+        console.log("My response", res.data)
+        return exits.ok(res.data)  
+      })
+      .catch((err) => {
+        console.log("This is an error", err)
+      })
+    // All done..
     return;
 
   }
